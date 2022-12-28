0fc5e3fe3870b540a54f1493d18b0c242b097da6
Luciano Bitencourt <lucbite@gmail.com>
2022-12-28 06:43:18 -0300

    update
    update

---
+++ js/script-while.js
@@ -0,0 +1,29 @@
+
+ function calcularTabuada(){
+
+     var vlr_num = document.getElementById('i-num').value
+     
+
+    
+    
+   
+     for (let i = 0; i < 10 ; i++) {
+        
+     
+        
+         document.write(` ${vlr_num} x ${i}= ${vlr_num*i}<br>`)
+
+        // console.log(` ${vlr_num} x ${i}= ${vlr_num*i}`);
+
+        // res.innerHTML = (`  ${i}`)
+        
+     }
+    
+        
+
+    
+
+ }
+
+
+    
